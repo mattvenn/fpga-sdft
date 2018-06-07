496cae101e6c50c076cad664a74b55718b2abecb
matt venn <matt@mattvenn.net>
2018-06-07 17:02:50 +0200

    fixed sdft and numpy.fft problems

--- python/sdft.py
+++ python/sdft.py
@@ -1,4 +1,4 @@
-########https://stackoverflow.com/questions/6663222/doing-fft-in-realtime
+# https://stackoverflow.com/questions/6663222/doing-fft-in-realtime
 from cmath import cos, sin, pi
 from scipy import signal
 import numpy as np
@@ -17,8 +17,7 @@ def init_coeffs():
     print(coeffs)
 
 
-def sdft():
-    delta = in_s[0] - in_s[N-1]
+def sdft(delta):
     for i in range(N):
         freqs[i] =  (freqs[i] + delta) * coeffs[i]
 
@@ -37,6 +36,7 @@ for i in range(N):
 # run the loop
 for i in range(N*40):
     # rotate in new sample
+    last = in_s[N-1]
     for i in range(N-1, 0, -1):
         in_s[i] = in_s[i-1]
     in_s[0] = complex(sig_in[sig_counter % N],0)
@@ -44,7 +44,8 @@ for i in range(N*40):
     sig_counter += 1
 
     # run the sdft
-    sdft()
+    delta = in_s[0] - last
+    sdft(delta)
 
 # plot the results and compare with numpy's fft
 import matplotlib.pyplot as plt
@@ -58,7 +59,7 @@ ax.plot(range(N), plot_freqs)
 ax.set_title("sliding dft")
 
 ax = fig.add_subplot(2,2,4)
-ax.plot(range(N), np.fft.fft(sig_in[0:N]))
+ax.plot(range(N), abs(np.fft.fft(sig_in[0:N])))
 ax.set_title("numpy fft")
 
 ax = fig.add_subplot(2,2,1)
